56484e4e07c967c2320bbb4ef897556d16f9ff65
AndreVazao <vazao.andre@gmail.com>
2026-05-16 07:46:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from flux.ui import start_app
+from flux.remote_server import start_server
+import threading
+
 
 if __name__ == "__main__":
+    # 🚀 Arranca servidor remoto (cockpit)
+    threading.Thread(target=start_server, daemon=True).start()
+
+    # 🖥️ Arranca UI normal
     start_app()
