69d088695c2411e2b8d9ed1bfcf761815a50eb5c
AndreVazao <vazao.andre@gmail.com>
2026-05-16 17:06:41 +0100

    Fix PyInstaller path handling in main.py
    Added path handling for PyInstaller executable.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import sys
+import os
+
+# 🔥 FIX PYINSTALLER PATH
+if getattr(sys, 'frozen', False):
+    sys.path.append(sys._MEIPASS)
+else:
+    sys.path.append(os.path.dirname(__file__))
+
 from flux.ui import start_app
 from flux.remote_server import start_server
 import threading
 
-
 if __name__ == "__main__":
-    # 🚀 Arranca servidor remoto (cockpit)
     threading.Thread(target=start_server, daemon=True).start()
-
-    # 🖥️ Arranca UI normal
     start_app()
