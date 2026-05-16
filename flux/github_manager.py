e90ae9585e812f0762807f6b0897ffa9c0ee43bc
AndreVazao <vazao.andre@gmail.com>
2026-05-16 06:51:23 +0100

    Implement GitHubManager class for repo management

---
+++ flux/github_manager.py
@@ -0,0 +1,28 @@
+from github import Github
+from flux.config import get_github_token
+
+class GitHubManager:
+
+    def __init__(self):
+        token = get_github_token()
+        if not token:
+            raise Exception("GITHUB_TOKEN not set")
+        self.gh = Github(token)
+        self.user = self.gh.get_user()
+
+    def create_repo(self, name):
+        return self.user.create_repo(name)
+
+    def create_branch(self, repo_name, new_branch, base="main"):
+        repo = self.user.get_repo(repo_name)
+        source = repo.get_branch(base)
+        repo.create_git_ref(ref=f"refs/heads/{new_branch}", sha=source.commit.sha)
+
+    def create_pr(self, repo_name, title, body, head, base="main"):
+        repo = self.user.get_repo(repo_name)
+        return repo.create_pull(title=title, body=body, head=head, base=base)
+
+    def merge_pr(self, repo_name, pr_number):
+        repo = self.user.get_repo(repo_name)
+        pr = repo.get_pull(pr_number)
+        return pr.merge()
